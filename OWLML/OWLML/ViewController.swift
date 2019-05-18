fe965b591087b90a373dfb37e08989da201ef349
Tyler Milner <tylermilner@gmail.com>
2019-05-18 18:04:41 -0500

    Basic example of showing document picker to select a video and then playing the video.

--- OWLML/OWLML/ViewController.swift
+++ OWLML/OWLML/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import AVKit
+import MobileCoreServices
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        presentDocumentPicker()
     }
 
-
+    private func playVideo(at url: URL) {
+        let player = AVPlayer(url: url)
+        
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = view.bounds
+        view.layer.addSublayer(playerLayer)
+        player.play()
+    }
+    
+    private func presentDocumentPicker() {
+        let types: [String] = [kUTTypeMPEG4 as String]
+        let documentPicker = UIDocumentPickerViewController(documentTypes: types, in: .import)
+        documentPicker.delegate = self
+        
+        present(documentPicker, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: UIDocumentPickerDelegate {
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let videoURL = urls.first else { return }
+        playVideo(at: videoURL)
+    }
+}
