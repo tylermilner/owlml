d4e12ad8b6b5928327753254535f5b4441e4c1f1
Tyler Milner <tylermilner@gmail.com>
2019-05-18 17:08:00 -0500

    Added an additional frame grab and templates for additional heroes based off the new frame grab (hence why "lucio.png" has changed). Now it appears only Zenyatta is being detected in this new frame grab, likely due to noise and subtle background differences of hero portraits between video frames.

--- owlml.py
+++ owlml.py
@@ -1,18 +1,29 @@
 import cv2
 import numpy as np
 
-img_rgb = cv2.imread('Images/Template Matching/frame_grab_cropped.png')
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+def findTemplate(template, img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    w, h = template.shape[::-1]
 
-template = cv2.imread('Images/Template Matching/lucio.png',0)
-w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where( res >= threshold)
 
-res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0,255,255), 1)
 
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 1)
+# img_rgb = cv2.imread('Images/Template Matching/frame_grab_cropped.png')
+img_rgb = cv2.imread('Images/Template Matching/frame_grab_cropped-2.png')
 
-cv2.imshow('Detected',img_rgb)
+lucioTemplate = cv2.imread('Images/Template Matching/lucio.png', 0)
+reinhardtTemplate = cv2.imread('Images/Template Matching/reinhardt.png', 0)
+zaryaTemplate = cv2.imread('Images/Template Matching/zarya.png', 0)
+zenyattaTemplate = cv2.imread('Images/Template Matching/zenyatta.png', 0)
+
+findTemplate(lucioTemplate, img_rgb)
+findTemplate(reinhardtTemplate, img_rgb)
+findTemplate(zaryaTemplate, img_rgb)
+findTemplate(zenyattaTemplate, img_rgb)
+
+cv2.imshow('Detected', img_rgb)
 cv2.waitKey()
